857118217134a5c8fbd77d80b0a7d86498be63c9
Ritvik BK <ritvikbk@gmail.com>
2026-08-30 13:32:55 +0530

    Add Week 3 Assignments

---
+++ Week 3/Problem3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem3 {
+
+    static void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = scanner.nextInt();
+
+        printNumbersUpToN(n);
+    }
+}
